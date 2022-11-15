868daa817dcd5e58285a1fadbb38aa07f9de3370
jhkim <jinhankim988@gmail.com>
2022-11-15 18:35:26 +0900

    Goods Builder

--- src/data/Goods.java
+++ src/data/Goods.java
@@ -1,13 +1,65 @@
 package data;
 
 public class Goods {
+    public static class Builder {
+        private String code;
+        private String groupName;
+        private String goodsName;
+
+        public Builder code(String code) {
+            this.code = code;
+            return this;
+        }
+
+        public Builder groupName(String groupName) {
+            this.groupName = groupName;
+            return this;
+        }
+
+        public Builder goodsName(String goodsName) {
+            this.goodsName = goodsName;
+            return this;
+        }
+
+        public Goods build() {
+            return new Goods(this);
+        }
+    }
     private String code;
+
+
+
     private String groupName;
     private String goodsName;
 
+
+    public Goods() {
+
+    }
     public Goods(String code, String groupName, String goodsName) {
         this.code = code;
         this.groupName =groupName;
         this.goodsName = goodsName;
+    }
+
+    public Goods(Builder builder) {
+        this.code = builder.code;
+        this.groupName = builder.groupName;
+        this.goodsName = builder.goodsName;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public String getCode() {
+        return code;
+    }
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public String getGoodsName() {
+        return goodsName;
     }
 }
