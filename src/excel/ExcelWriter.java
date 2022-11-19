8874f2cabf567e56f304cb4a5ba36183ed3a36cc
jhkim988 <fccva3910@gmail.com>
2022-11-19 19:53:08 +0900

    리팩토링

--- src/excel/ExcelWriter.java
+++ src/excel/ExcelWriter.java
@@ -8,19 +8,33 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class ExcelWriter {
+    private int rowNum = 0;
+    private int colNum = 0;
     private String filePath;
     private String fileName;
     private XSSFWorkbook workbook;
     private XSSFSheet sheet;
 
-    public void setCell() {
-        int rowNumber = 0;
-        int cellNumber = 0;
-        Row row = sheet.createRow(rowNumber++);
-        Cell cell = row.createCell(cellNumber++);
-        cell.setCellValue("value");
+    private Row row;
+    private Cell cell;
+
+    public ExcelWriter(String filePath, String fileName) {
+        this.filePath = filePath;
+        this.fileName = fileName;
+        this.workbook = new XSSFWorkbook();
+        this.sheet = workbook.createSheet();
+    }
+
+    public void createRow() {
+        colNum = 0;
+        this.row = sheet.createRow(rowNum++);
+    }
+
+    public Cell getNextCell() {
+        return row.createCell(colNum++);
     }
 
     public void save() {
