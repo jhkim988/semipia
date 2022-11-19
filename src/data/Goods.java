22543f9df22780c7fb9ba8a5a2b46bdaa4899b20
jhkim988 <fccva3910@gmail.com>
2022-11-19 19:53:21 +0900

    불필요한 생성자 삭제

--- src/data/Goods.java
+++ src/data/Goods.java
@@ -5,9 +5,6 @@ public class Goods {
     private String groupName;
     private String goodsName;
 
-    public Goods() {
-
-    }
     public Goods(String code, String groupName, String goodsName) {
         this.code = code;
         this.groupName =groupName;
