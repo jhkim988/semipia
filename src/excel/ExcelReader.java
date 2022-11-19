9d99a4919c5c5652793fbd92a8724b1207ebfdb9
jhkim988 <fccva3910@gmail.com>
2022-11-19 19:52:59 +0900

    리팩토링

--- src/excel/ExcelReader.java
+++ src/excel/ExcelReader.java
@@ -3,6 +3,7 @@ package excel;
 import data.Goods;
 import data.SaleInfo;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -16,8 +17,8 @@ import java.util.stream.Collectors;
 
 public class ExcelReader {
     @FunctionalInterface
-    private interface Actor {
-        void act(XSSFSheet sheet);
+    public interface Actor {
+        void act(Row row);
     }
     private final String filePath;
     private final String fileName;
@@ -35,45 +36,21 @@ public class ExcelReader {
         try (FileInputStream fis = new FileInputStream(new File(filePath, fileName))) {
             XSSFWorkbook workbook = new XSSFWorkbook(fis);
             XSSFSheet sheet = workbook.getSheet(sheetName);
-            actor.act(sheet);
+            boolean isFirst = true;
+            for (Row row : sheet) {
+                if (isFirst) {
+                    isFirst = false;
+                    continue;
+                }
+                actor.act(row);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        final Map<String, Goods> goodsMap = new HashMap<>();
-        new ExcelReader(
-                "../2022-11-13"
-                , "0.품목리스트.xlsx"
-                , "품목등록"
-                , sheet -> {
-            for (Row row : sheet) {
-                Iterator<Cell> iter = row.iterator();
-                String code = iter.next().getStringCellValue();
-                String groupName = iter.next().getStringCellValue();
-                String goodsName = iter.next().getStringCellValue();
-                goodsMap.put(code, new Goods(code, groupName, goodsName));
-            }
-        }).load();
-
-        final List<SaleInfo> saleInfoList = new ArrayList<>();
-        new ExcelReader(
-                "../2022-11-13"
-                , "1.판매현황.xlsx"
-                , "판매현황"
-                , sheet -> {
-                    for (Row row : sheet) {
-                        Iterator<Cell> iter = row.iterator();
-                        String goodsName = iter.next().getStringCellValue();
-                        LocalDate localDate = iter.next().getLocalDateTimeCellValue().toLocalDate();
-                        String partner = iter.next().getStringCellValue();
-                        int quantity = (int) iter.next().getNumericCellValue();
-                        saleInfoList.add(new SaleInfo(goodsName, localDate, partner, quantity));
-                    }
-        }
-        ).load();
-
-        saleInfoList.stream().collect(Collectors.groupingBy(SaleInfo::getGoodsName));
+    public static int numericalElseZero(Cell cell) {
+        if (cell.getCellType().equals(CellType.NUMERIC)) return (int) cell.getNumericCellValue();
+        return 0;
     }
 }
