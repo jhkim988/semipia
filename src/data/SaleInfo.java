0503a7502f4d0ef92d4febd4d580f6de71d85dee
jhkim988 <fccva3910@gmail.com>
2022-11-19 19:51:03 +0900

    메서드 추가

--- src/data/SaleInfo.java
+++ src/data/SaleInfo.java
@@ -2,20 +2,34 @@ package data;
 
 import java.time.LocalDate;
 
-public class SaleInfo {
-    private String goodsName;
-    private LocalDate date;
-    private String partner;
-    private int quantity;
+public class SaleInfo implements Comparable<SaleInfo> {
+    private final String partner;
+    private final int quantity;
 
-    public SaleInfo(String goodsName, LocalDate date, String partner, int quantity) {
-        this.goodsName = goodsName;
-        this.date = date;
+    public SaleInfo(String partner, int quantity) {
         this.partner = partner;
         this.quantity = quantity;
     }
 
-    public String getGoodsName() {
-        return this.goodsName;
+    @Override
+    public int compareTo(SaleInfo o) {
+        return Integer.compare(o.quantity, this.quantity);
     }
+
+    @Override
+    public String toString() {
+        return "SaleInfo{" +
+                "partner='" + partner + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+
+    public String getPartner() {
+        return partner;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
 }
