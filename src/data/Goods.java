d473f9cadda03234be70d4a47cdb51e9e958def4
jhkim <jinhankim988@gmail.com>
2022-11-19 09:22:22 +0900

    delete builder

--- src/data/Goods.java
+++ src/data/Goods.java
@@ -1,37 +1,9 @@
 package data;
 
 public class Goods {
-    public static class Builder {
-        private String code;
-        private String groupName;
-        private String goodsName;
-
-        public Builder code(String code) {
-            this.code = code;
-            return this;
-        }
-
-        public Builder groupName(String groupName) {
-            this.groupName = groupName;
-            return this;
-        }
-
-        public Builder goodsName(String goodsName) {
-            this.goodsName = goodsName;
-            return this;
-        }
-
-        public Goods build() {
-            return new Goods(this);
-        }
-    }
     private String code;
-
-
-
     private String groupName;
     private String goodsName;
-
 
     public Goods() {
 
@@ -41,17 +13,6 @@ public class Goods {
         this.groupName =groupName;
         this.goodsName = goodsName;
     }
-
-    public Goods(Builder builder) {
-        this.code = builder.code;
-        this.groupName = builder.groupName;
-        this.goodsName = builder.goodsName;
-    }
-
-    public static Builder builder() {
-        return new Builder();
-    }
-
     public String getCode() {
         return code;
     }
