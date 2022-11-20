99e9076a1cf4b9080c0edd795927d319d1e17545
jhkim988 <fccva3910@gmail.com>
2022-11-20 22:13:24 +0900

    Double 로 타입 변경, 이익률 관련 추가

--- src/data/Goods.java
+++ src/data/Goods.java
@@ -1,15 +1,30 @@
 package data;
 
 public class Goods {
-    private String code;
-    private String groupName;
-    private String goodsName;
+    private final String code;
+    private final String groupName;
+    private final String goodsName;
+
+    private Double salePrice = 0.0;
+    private Double productionPrice = 0.0;
+    private Double currentQuantity = 0.0;
 
     public Goods(String code, String groupName, String goodsName) {
         this.code = code;
         this.groupName =groupName;
         this.goodsName = goodsName;
     }
+
+    public Double calculateMargin() {
+        return salePrice - productionPrice;
+    }
+    public Double calculateMarginRate() {
+        return (salePrice - productionPrice)/productionPrice;
+    }
+public Double calculateCurrentTotalStockProductionPrice() {
+        return currentQuantity * productionPrice;
+}
+
     public String getCode() {
         return code;
     }
@@ -19,5 +34,28 @@ public class Goods {
 
     public String getGoodsName() {
         return goodsName;
+    }
+
+    public Double getSalePrice() {
+        return salePrice;
+    }
+
+    public Double getProductionPrice() {
+        return productionPrice;
+    }
+
+    public void setSalePrice(Double salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public void setProductionPrice(Double productionPrice) {
+        this.productionPrice = productionPrice;
+    }
+    public Double getCurrentQuantity() {
+        return currentQuantity;
+    }
+
+    public void setCurrentQuantity(Double currentQuantity) {
+        this.currentQuantity = currentQuantity;
     }
 }
